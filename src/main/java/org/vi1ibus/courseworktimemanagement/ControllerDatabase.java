e3aa4668fb3933657fe33845d34a9d95b305c633
vir1ibus <vir1ibus@gmail.com>
2021-10-09 15:16:00 +0300

    ControllerDatabase: Reworked database queries, fixed the possibility of SQL injection. MainApplication, AuthenticationScreenController: Removed unnecessary initialization of variables. Added centralization of main variables in the main class of the program.

--- src/main/java/org/vi1ibus/courseworktimemanagement/ControllerDatabase.java
+++ src/main/java/org/vi1ibus/courseworktimemanagement/ControllerDatabase.java
@@ -6,19 +6,19 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class ControllerDatabase {
-    private static String url="jdbc:mysql://localhost:3306/?useSSL=true&serverTimezone=UTC";
-    private static final String username="vir1ibus";
-    private static final String password="vir1ibus";
+    private static String url="jdbc:mysql://localhost:3306/?useSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String username="root";
+    private static final String password="root";
     private static Connection connection;
-    private static Statement statement;
+    private static PreparedStatement statement;
 
-    public ControllerDatabase(){
+    public int checkExistsDB(){
         try {
             connectDB();
+            Statement statement = connection.createStatement();
             statement.executeUpdate("CREATE DATABASE IF NOT EXISTS app;");
-            url = "jdbc:mysql://localhost:3306/app?useSSL=true&serverTimezone=UTC";
+            url = "jdbc:mysql://localhost:3306/app?useSSL=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
             connection = DriverManager.getConnection(url, username, password);
-            statement = connection.createStatement();
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS `statuses`(" +
                     "  `id` int NOT NULL," +
                     "  `name` varchar(45) NOT NULL," +
@@ -76,24 +76,26 @@ public class ControllerDatabase {
                     "  ADD CONSTRAINT `fk_tasks_list_has_users_tasks_list` FOREIGN KEY (`tasks_list_id`) REFERENCES `tasks_list` (`id`),\n" +
                     "  ADD CONSTRAINT `fk_tasks_list_has_users_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);"
             );
+            return 0;
         } catch (SQLException | ClassNotFoundException e){
-            return;
+            return -1;
         }
     }
 
     public static void connectDB() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         connection= DriverManager.getConnection(url, username, password);
-        statement = connection.createStatement();
     }
 
     public static User authenticationUser(String login, String password) {
         try {
             connectDB();
-            String query = "SELECT * from users WHERE login='" + login + "' and password='" + password + "';";
-            ResultSet resultSet = statement.executeQuery(query);
+            String query = "SELECT * from users WHERE login=? and password=?;";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, login);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
             resultSet.next();
-            int id = Integer.parseInt(resultSet.getString(1));
             return new User(Integer.parseInt(resultSet.getString(1)), resultSet.getString(2), resultSet.getString(4));
         } catch (SQLException | ClassNotFoundException e){
             e.printStackTrace();
@@ -104,8 +106,12 @@ public class ControllerDatabase {
     public static int registrationUser(String login, String password, String email){
         try {
             connectDB();
-            String query = "INSERT INTO users(login, password, email) VALUES ('" + login + "', '" + password + "', '" + email +"');";
-            statement.executeUpdate(query);
+            String query = "INSERT INTO users(login, password, email) VALUES (?, ?, ?);";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, login);
+            statement.setString(2, password);
+            statement.setString(3, email);
+            statement.executeUpdate();
             return 0;
         } catch (SQLIntegrityConstraintViolationException e){
             if(e.toString().contains("login")){
@@ -126,8 +132,10 @@ public class ControllerDatabase {
     public static boolean checkExistenceUserLogin(String login){
         try {
             connectDB();
-            String query = "SELECT * from users WHERE login='" + login + "';";
-            ResultSet resultSet = statement.executeQuery(query);
+            String query = "SELECT * from users WHERE login=?;";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, login);
+            ResultSet resultSet = statement.executeQuery();
             resultSet.next();
             resultSet.getString(1);
             return true;
@@ -139,8 +147,10 @@ public class ControllerDatabase {
     public static boolean checkExistenceUserEmail(String email){
         try {
             connectDB();
-            String query = "SELECT * from users WHERE email='" + email + "';";
-            ResultSet resultSet = statement.executeQuery(query);
+            String query = "SELECT * from users WHERE email=?;";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, email);
+            ResultSet resultSet = statement.executeQuery();
             resultSet.next();
             resultSet.getString(1);
             return true;
@@ -152,11 +162,12 @@ public class ControllerDatabase {
     public static void createTaskList(TaskList taskList){
         try {
             connectDB();
-            String query = "INSERT INTO tasks_list(name, everyone, owner) VALUES ('"
-                    + taskList.getName() + "', "
-                    + taskList.getEveryone() + ", "
-                    + taskList.getOwner() + ");";
-            statement.executeUpdate(query);
+            String query = "INSERT INTO tasks_list(name, everyone, owner) VALUES (?, ?, ?);";
+            statement = connection.prepareStatement(query);
+            statement.setString(1, taskList.getName());
+            statement.setInt(2, taskList.getEveryone());
+            statement.setInt(3, taskList.getOwner());
+            statement.executeUpdate();
         } catch (SQLException | ClassNotFoundException e){
             return;
         }
@@ -165,9 +176,10 @@ public class ControllerDatabase {
     public static TaskList getTaskList(int taskListId){
         try {
             connectDB();
-            String query = "SELECT * FROM tasks_list WHERE id=" + taskListId + ";";
-            ResultSet resultSet = statement.executeQuery(query);
-
+            String query = "SELECT * FROM tasks_list WHERE id=?;";
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, taskListId);
+            ResultSet resultSet = statement.executeQuery();
             return new TaskList(
                     Integer.parseInt(resultSet.getString(1)),
                     resultSet.getString(2),
@@ -183,13 +195,17 @@ public class ControllerDatabase {
     public static ArrayList<Pair<String, Integer>> getTaskListStatuses(int taskListId) {
         try {
             connectDB();
-            String query = "SELECT id, name FROM statuses WHERE tasks_list_id=" + taskListId + ";";
-            ResultSet resultSetNames = statement.executeQuery(query);
-
+            String query = "SELECT id, name FROM statuses WHERE tasks_list_id=?;";
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, taskListId);
+            ResultSet resultSetNames = statement.executeQuery();
             ArrayList<Pair<String, Integer>> arrayList = new ArrayList<>();
             while (resultSetNames.next()){
-                query = "SELECT count(*) FROM task WHERE tasks_list_id=" + taskListId + " and status="+ resultSetNames.getString(1) + ";";
-                ResultSet resultSetCount = statement.executeQuery(query);
+                query = "SELECT count(*) FROM task WHERE tasks_list_id=? AND status=?;";
+                statement = connection.prepareStatement(query);
+                statement.setInt(1, taskListId);
+                statement.setString(1, resultSetNames.getString(1) ); // FIX
+                ResultSet resultSetCount = statement.executeQuery();
                 arrayList.add(new Pair<>(resultSetNames.getString(2), Integer.parseInt(resultSetCount.getString(1))));
             }
             return arrayList;
